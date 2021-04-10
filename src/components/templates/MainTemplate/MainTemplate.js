70580f0ab19f6d2b382e2e48498e688ba40a37bb
Donquixote Sabo <mixboksu@gmail.com>
2021-04-10 10:49:20 +0200

    feat: :sparkles: Add SearchBar

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,19 +1,9 @@
 import React from 'react';
+//components
 import Navigation from 'components/organisms/Navigation/Navigation';
-import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles';
-import styled from 'styled-components';
-
-const SearchBar = styled.div`
-  grid-row: 1 / 2;
-  grid-column: 2 / 3;
-  border: 1px solid ${({ theme }) => theme.colors.darkPurple};
-`;
-
-const News = styled.div`
-  grid-row: 1 / 3;
-  grid-column: 3 / 3;
-  border: 1px solid ${({ theme }) => theme.colors.darkPurple};
-`;
+import SearchBar from 'components/organisms/SearchBar/SearchBar';
+//styles
+import { Wrapper, News } from 'components/templates/MainTemplate/MainTemplate.styles';
 
 const MainTemplate = ({ children }) => {
   return (
--- src/components/templates/MainTemplate/MainTemplate.styles.js
+++ src/components/templates/MainTemplate/MainTemplate.styles.js
@@ -11,3 +11,9 @@ export const Wrapper = styled.div`
   grid-template-columns: 150px 1fr 0.75fr;
   background-color: ${({ theme }) => theme.colors.lightGrey};
 `;
+
+export const News = styled.div`
+  grid-row: 1 / 3;
+  grid-column: 3 / 3;
+  border: 1px solid ${({ theme }) => theme.colors.darkPurple};
+`;
