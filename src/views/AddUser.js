bcedbab1d7ba44862c30bcba64de05bc0d86a1fa
Donquixote Sabo <mixboksu@gmail.com>
2021-04-10 10:17:54 +0200

    Add useWindowSize hook

--- src/views/AddUser.js
+++ src/views/AddUser.js
@@ -4,6 +4,7 @@ import { Button } from 'components/atoms/Button/Button';
 import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
 import { Title } from 'components/atoms/Title/Title';
 import { UsersContext } from 'providers/UsersProvider';
+import { useWindowHeight } from 'hooks/useWindowsSize';
 
 const initialFormState = {
   name: '',
@@ -11,14 +12,19 @@ const initialFormState = {
   average: '',
 };
 
+const actionTypes = {
+  INPUT_CHANGE: 'INPUT_CHANGE',
+  CLEAR_VALUES: 'CLEAR_VALUES',
+};
+
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'INPUT_CHANGE':
+    case actionTypes.INPUT_CHANGE:
       return {
         ...state,
         [action.payload.field]: action.payload.value,
       };
-    case 'CLEAR_VALUES':
+    case actionTypes.CLEAR_VALUES:
       return initialFormState;
     default:
       return state;
@@ -28,10 +34,11 @@ const reducer = (state, action) => {
 const AddUser = () => {
   const [formValues, dispatch] = useReducer(reducer, initialFormState);
   const { handleAddUser } = useContext(UsersContext);
+  const dimension = useWindowHeight();
 
   const handleInputChange = (e) => {
     dispatch({
-      type: 'INPUT_CHANGE',
+      type: actionTypes.INPUT_CHANGE,
       payload: {
         field: e.target.name,
         value: e.target.value,
@@ -42,12 +49,14 @@ const AddUser = () => {
   const handleSubmitUser = (e) => {
     e.preventDefault();
     handleAddUser(formValues);
-    dispatch({ type: 'CLEAR_VALUES' });
+    dispatch({ type: actionTypes.CLEAR_VALUES });
   };
 
   return (
     <ViewWrapper as="form" onSubmit={handleSubmitUser}>
       <Title>Add new student</Title>
+      <Title>{dimension.width}</Title>
+      <Title>{dimension.height}</Title>
       <FormField label="Name" id="name" name="name" value={formValues.name} onChange={handleInputChange} />
       <FormField label="Attendance" id="attendance" name="attendance" value={formValues.attendance} onChange={handleInputChange} />
       <FormField label="Average" id="average" name="average" value={formValues.average} onChange={handleInputChange} />
