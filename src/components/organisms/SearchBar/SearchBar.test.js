c622e37a854f6ade82e4007a3be49280ec54b720
Donquixote Sabo <mixboksu@gmail.com>
2021-04-15 07:03:42 +0200

    test: :white_check_mark: Test component rendering and searching users

---
+++ src/components/organisms/SearchBar/SearchBar.test.js
@@ -0,0 +1,27 @@
+import { render, screen, fireEvent } from 'test-utils';
+import { setupServer } from 'msw/node';
+//MSW handlers
+import { handlers } from 'mocks/handlers';
+//components
+import { SearchBar } from './SearchBar';
+
+const server = setupServer(...handlers);
+
+describe('Search Bar', () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+  it('Renders the component', () => {
+    render(<SearchBar />);
+    screen.getByText('Teacher');
+    screen.getByPlaceholderText('Search');
+  });
+  it('Displays users when search phrase is matching', async () => {
+    render(<SearchBar />);
+    const input = screen.getByPlaceholderText('Search');
+    fireEvent.change(input, { target: { value: 'ad' } });
+
+    await screen.findByText(/Adam Romański/);
+  });
+});
