6698a97b7e405ea7b2bdcd9e88f0a5f528d31b02
Donquixote Sabo <mixboksu@gmail.com>
2021-04-19 07:04:45 +0200

    feat: :memo: Add stories for all atoms

---
+++ src/components/atoms/Input/Input.stories.js
@@ -0,0 +1,10 @@
+import { Input } from './Input';
+
+export default {
+  title: 'Components/Atoms/Input',
+  component: Input,
+};
+
+const Template = (args) => <Input {...args} />;
+
+export const Default = Template.bind({});
