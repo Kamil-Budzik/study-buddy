e4b8d6bb761faae5c43ed2c108c859f3bc13a780
Donquixote Sabo <mixboksu@gmail.com>
2021-05-10 07:05:47 +0200

    refactor: :pencil2: Delete User type

--- src/components/molecules/StudentsListItem/StudentsListItem.js
+++ src/components/molecules/StudentsListItem/StudentsListItem.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 //components
 import DeleteButton from 'components/atoms/DeleteButton/DeleteButton';
 import { Average } from 'components/atoms/Average/Average';
-//types
-import { UserShape } from 'types';
 //styles
 import { StyledInfo, Wrapper } from './StudentsListItem.styles';
 
@@ -24,10 +21,6 @@ const StudentsListItem = ({
       </StyledInfo>
     </Wrapper>
   );
-};
-
-StudentsListItem.propTypes = {
-  userData: PropTypes.shape(UserShape),
 };
 
 export default StudentsListItem;
