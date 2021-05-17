d05ca3308fc0a89a9f8cde959714e02d2bdbdae8
Donquixote Sabo <mixboksu@gmail.com>
2021-05-17 06:32:38 +0200

    feat: :sparkles: Add React-hook-form

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { theme } from 'assets/styles/theme';
@@ -11,24 +12,60 @@ import {
 } from 'react-router-dom';
 import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
 import Dashboard from 'views/Dashboard';
+import FormField from 'components/molecules/FormField/FormField';
+import { Button } from 'components/atoms/Button/Button';
+
+const AuthenticatedApp = () => {
+  return (
+    <MainTemplate>
+      <Wrapper>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/group" />
+          </Route>
+          <Route path="/group/:id?">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Wrapper>
+    </MainTemplate>
+  );
+};
+
+const UnauthenticatedApp = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <FormField label="login" name="login" id="login" {...register('login')} />
+      <FormField
+        label="password"
+        name="password"
+        id="password"
+        type="password"
+        {...register('password')}
+      />
+      <Button>Sign in</Button>
+    </form>
+  );
+};
 
 const Root = () => {
+  const [user, setUser] = React.useState(null);
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <MainTemplate>
-          <Wrapper>
-            <Switch>
-              <Route exact path="/">
-                <Redirect to="/group" />
-              </Route>
-              <Route path="/group/:id?">
-                <Dashboard />
-              </Route>
-            </Switch>
-          </Wrapper>
-        </MainTemplate>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
       </ThemeProvider>
     </Router>
   );
